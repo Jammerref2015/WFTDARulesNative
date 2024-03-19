77e02a4f2725ac13aa646e29a4a41e6f2bdeec23
Jammerref2015 <Adrian.looney@gmail.com>
2024-03-19 16:50:04 +0000

    Setup App Installation

---
+++ TabApp/Screens/Summary.js
@@ -0,0 +1,14 @@
+// screens/Summary.js
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Summary = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Summary!</Text>
+    </View>
+  );
+}
+
+export default Summary;
